525745bb9fdfe0583aff2f69fe87f4b1fb0a8537
Boyzmsc <boyzmsc@naver.com>
2021-01-11 23:43:44 +0900

    feat: Update TodoList.vue

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -29,6 +29,7 @@ body {
 }
 input {
     border-style: groove;
+    width: 200px;
 }
 
 button {
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-      List
+      <ul>
+        <li v-for = "item in todoItems" v-bind:key = "item">
+          {{item}}
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function(){
+    return {
+      todoItems : []
+    }
+  },
+  created: function(){
+    if (localStorage.length > 0){
+      for (var i = 0; i < localStorage.length; i++){
+        // 웹팩 정보 제외 구문
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          console.log(localStorage.key(i));
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
 }
 </script>
 
