d6dd87c9cedbc5d0d439de085fa3f88216085616
Boyzmsc <boyzmsc@naver.com>
2021-01-11 21:24:10 +0900

    feat: Add components

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <TodoHeader></TodoHeader>
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
+        <TodoFooter></TodoFooter>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components : {
+        'TodoHeader' : TodoHeader,
+        'TodoInput' : TodoInput,
+        'TodoList' : TodoList,
+        'TodoFooter' : TodoFooter
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Input
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
