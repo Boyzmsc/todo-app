ec7e1d6ff479e2ffbd59001bf038fc532b6097e8
Boyzmsc <boyzmsc@naver.com>
2021-01-11 22:50:34 +0900

    feat: Add Input func

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-      Input
+      <input type="text" v-model = "newTodoItem">
+      <button v-on:click = "addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data : function(){
+    return {
+      newTodoItem : ""
+    }
+  },
+  methods: {
+    addTodo : function(){
+      console.log(this.newTodoItem);
+      // 저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      // input box 초기화
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
