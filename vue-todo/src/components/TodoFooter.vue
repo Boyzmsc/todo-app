8e75ee3eb5b524fdc5626bc05421f18760f9d8b4
Boyzmsc <boyzmsc@naver.com>
2021-01-12 21:07:41 +0900

    feat: Update footer UI

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-      Footer
+      <span>
+        Clear All
+      </span>
   </div>
 </template>
 
@@ -10,6 +12,17 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.clearAllContainer{
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn{
+  color:#e20303;
+  display: block;
+}
 </style>
